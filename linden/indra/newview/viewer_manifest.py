9bced5eac4d1928f87fdc70a51e0eb76109b52ed
Jacek Antonelli <jacek.antonelli@gmail.com>
2008-09-19 16:51:37 -0500

    VWR-2865: New SConstruct flag to (not) make tarball after compiling. For convenience until we switch to CMake.

--- linden/indra/newview/viewer_manifest.py
+++ linden/indra/newview/viewer_manifest.py
@@ -483,11 +483,14 @@ class LinuxManifest(ViewerManifest):
                 find %(dst)s -type f -perm 0400 | xargs chmod 0444;
                 true""" %  {'dst':self.get_dst_prefix() })
 
-                # temporarily move directory tree so that it has the right name in the tarfile
-                self.run_command("mv %(dst)s %(inst)s" % {'dst':self.get_dst_prefix(),'inst':self.src_path_of(installer_name)})
-                # --numeric-owner hides the username of the builder for security etc.
-                self.run_command('tar -C %(dir)s --numeric-owner -cjf %(inst_path)s.tar.bz2 %(inst_name)s' % {'dir':self.get_src_prefix(), 'inst_name': installer_name, 'inst_path':self.src_path_of(installer_name)})
-                self.run_command("mv %(inst)s %(dst)s" % {'dst':self.get_dst_prefix(),'inst':self.src_path_of(installer_name)})
+                if("package" in self.args['actions'] or
+                   "unpacked" in self.args['actions']):
+                        # temporarily move directory tree so that it has the right name in the tarfile
+                        self.run_command("mv %(dst)s %(inst)s" % {'dst':self.get_dst_prefix(),'inst':self.src_path_of(installer_name)})
+                        # --numeric-owner hides the username of the builder for security etc.
+                        self.run_command('tar -C %(dir)s --numeric-owner -cjf %(inst_path)s.tar.bz2 %(inst_name)s' % {'dir':self.get_src_prefix(), 'inst_name': installer_name, 'inst_path':self.src_path_of(installer_name)})
+                        self.run_command("mv %(inst)s %(dst)s" % {'dst':self.get_dst_prefix(),'inst':self.src_path_of(installer_name)})
+
 
 class Linux_i686Manifest(LinuxManifest):
         def construct(self):
