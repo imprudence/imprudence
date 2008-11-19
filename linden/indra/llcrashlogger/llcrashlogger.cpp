317612f9e2adb134bb3006621c81d40c3508f776
Jacek Antonelli <jacek.antonelli@gmail.com>
2008-11-18 22:01:48 -0600

    Changed to Imprudence.exec_marker to match llappviewer.cpp.

--- linden/indra/llcrashlogger/llcrashlogger.cpp
+++ linden/indra/llcrashlogger/llcrashlogger.cpp
@@ -341,7 +341,7 @@ bool LLCrashLogger::init()
 	//Run through command line options
 	if(getOption("previous").isDefined())
 	{
-		llinfos << "Previous execution did not remove SecondLife.exec_marker" << llendl;
+		llinfos << "Previous execution did not remove Imprudence.exec_marker" << llendl;
 		mCrashInPreviousExec = TRUE;
 	}
 
@@ -371,7 +371,7 @@ bool LLCrashLogger::init()
 	//If we've opened the crash logger, assume we can delete the marker file if it exists	
 	if( gDirUtilp )
 	{
-		std::string marker_file = gDirUtilp->getExpandedFilename(LL_PATH_LOGS,"SecondLife.exec_marker");
+		std::string marker_file = gDirUtilp->getExpandedFilename(LL_PATH_LOGS,"Imprudence.exec_marker");
 		ll_apr_file_remove( marker_file );
 	}
 	
