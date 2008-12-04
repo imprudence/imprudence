da34ca62889beb4b88d204e32e5db60e139beeb0
McCabe Maxsted <hakushakukun@gmail.com>
2008-12-04 15:09:42 -0700

    Fixed localsim login url

--- linden/indra/newview/llviewernetwork.cpp
+++ linden/indra/newview/llviewernetwork.cpp
@@ -60,7 +60,7 @@ static LLGridData gGridInfo[GRID_INFO_COUNT] =
 	{ "Local OpenSim",
 	  "",
 	  "localhost",
-	  "http://0.0.0.0:9000",
+	  "http://127.0.0.1:9000",
 	  "" },
 	{ "Other", "", "", "", "" }
 };
