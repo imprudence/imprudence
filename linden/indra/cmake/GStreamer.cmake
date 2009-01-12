d971dae8333841de1d4392f046d304917f9e31c1
Jacek Antonelli <jacek.antonelli@gmail.com>
2009-01-11 18:43:40 -0600

    Added more gstreamer include paths.

--- linden/indra/cmake/GStreamer.cmake
+++ linden/indra/cmake/GStreamer.cmake
@@ -15,6 +15,8 @@ elseif (LINUX)
   set(GSTREAMER_INCLUDE_DIRS
       ${LIBS_PREBUILT_DIR}/${LL_ARCH_DIR}/include/gstreamer-0.10
       ${LIBS_PREBUILT_DIR}/${LL_ARCH_DIR}/include/glib-2.0
+      ${LIBS_PREBUILT_DIR}/${LL_ARCH_DIR}/include/glib-2.0/glib
+      ${LIBS_PREBUILT_DIR}/${LL_ARCH_DIR}/include/glib-2.0/gobject
       ${LIBS_PREBUILT_DIR}/${LL_ARCH_DIR}/include/libxml2
       )
   # We don't need to explicitly link against gstreamer itself, because
