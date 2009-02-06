40811dbf0d99f07acaedcd8c0f01803b39971df9
McCabe Maxsted <hakushakukun@gmail.com>
2009-02-06 01:49:25 -0700

    develop.py should now work again under windows

--- linden/indra/cmake/OPENAL.cmake
+++ linden/indra/cmake/OPENAL.cmake
@@ -12,7 +12,7 @@ if (OPENAL)
   if (WINDOWS)
   find_library(OPENAL_LIB
     NAMES openal openal32 OpenAL OpenAL32 wrap_oal
-    PATHS ${LIBS_PREBUILT_DIR}/${LL_ARCH_DIR}/libraries/i686-win32/lib/release/
+    PATHS ${CMAKE_SOURCE_DIR}/../libraries/i686-win32/lib/release
     )
 
   else (WINDOWS)
@@ -20,7 +20,8 @@ if (OPENAL)
     NAMES openal openal32 OpenAL OpenAL32 wrap_oal
     PATHS ${ARCH_PREBUILT_DIRS_RELEASE}
     )
-
+  endif (WINDOWS)
+  
   if (NOT OPENAL_LIB)
     message(FATAL_ERROR "OpenAL not found!")
   else (NOT OPENAL_LIB)
@@ -46,10 +47,18 @@ if (OPENAL)
 
   # ALUT_LIB
 
+  if (WINDOWS)
+   find_library(ALUT_LIB
+   NAMES alut freealut
+   PATHS ${CMAKE_SOURCE_DIR}/../libraries/i686-win32/lib/release
+   )
+  
+  else (WINDOWS) 
   find_library(ALUT_LIB
     NAMES alut freealut
     PATHS ${ARCH_PREBUILT_DIRS_RELEASE}
     )
+  endif (WINDOWS)
 
   if (NOT ALUT_LIB)
     message(FATAL_ERROR "ALUT not found!")
