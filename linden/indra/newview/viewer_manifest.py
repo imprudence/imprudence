d5cfd302587a6eaaafc2a5d06763b1b9366a7873
Jacek Antonelli <jacek.antonelli@gmail.com>
2009-03-07 22:07:58 -0600

    Moved Info-SecondLife.plist to Info-Imprudence.plist

--- linden/indra/newview/viewer_manifest.py
+++ linden/indra/newview/viewer_manifest.py
@@ -403,7 +403,7 @@ class DarwinManifest(ViewerManifest):
             #  <bundle>/Contents/MacOS/
             self.contents_of_tar(self.args['source']+'/mozilla-universal-darwin.tgz', 'MacOS')
 
-            self.path("Info-SecondLife.plist", dst="Info.plist")
+            self.path("Info-Imprudence.plist", dst="Info.plist")
 
             # copy additional libs in <bundle>/Contents/MacOS/
             self.path("../../libraries/universal-darwin/lib_release/libndofdev.dylib", dst="MacOS/libndofdev.dylib")
