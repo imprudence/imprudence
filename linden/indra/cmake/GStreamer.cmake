94ba8cdc2ab88697abb339829590776ec9b2843e
Jacek Antonelli <jacek.antonelli@gmail.com>
2009-03-08 14:59:57 -0500

    Tweaked GStreamer.cmake to work on Mac

--- linden/indra/cmake/GStreamer.cmake
+++ linden/indra/cmake/GStreamer.cmake
@@ -54,10 +54,6 @@ if (WINDOWS)
 
 else (WINDOWS)
 
-  include(FindPkgConfig)
-
-  pkg_check_modules(GSTREAMER REQUIRED gstreamer-0.10)
-  pkg_check_modules(GSTREAMER_PLUGINS_BASE REQUIRED gstreamer-plugins-base-0.10)
   set(GSTREAMER_INCLUDE_DIRS
       ${LIBS_PREBUILT_DIR}/${LL_ARCH_DIR}/include/gstreamer-0.10
       ${LIBS_PREBUILT_DIR}/${LL_ARCH_DIR}/include/glib-2.0
@@ -66,16 +62,36 @@ else (WINDOWS)
       ${LIBS_PREBUILT_DIR}/${LL_ARCH_DIR}/include/libxml2
       )
 
-  # We don't need to explicitly link against gstreamer itself, because
-  # LLMediaImplGStreamer probes for the system's copy at runtime.
-  set(GSTREAMER_LIBRARIES
-      gobject-2.0
-      gmodule-2.0
-      dl
-      gthread-2.0
-      rt
-      glib-2.0
-      )
+  if (DARWIN)
+
+    set(GSTREAMER_LIBRARIES
+        xml2
+        glib-2.0
+        gobject-2.0
+        gmodule-2.0
+        gthread-2.0
+        gstreamer-0.10
+        )
+
+  else (DARWIN)
+
+    include(FindPkgConfig)
+
+    pkg_check_modules(GSTREAMER REQUIRED gstreamer-0.10)
+    pkg_check_modules(GSTREAMER_PLUGINS_BASE REQUIRED gstreamer-plugins-base-0.10)
+
+    # We don't need to explicitly link against gstreamer itself, because
+    # LLMediaImplGStreamer probes for the system's copy at runtime.
+    set(GSTREAMER_LIBRARIES
+        gobject-2.0
+        gmodule-2.0
+        dl
+        gthread-2.0
+        rt
+        glib-2.0
+        )
+
+  endif (DARWIN)
 
 endif (WINDOWS)
 
