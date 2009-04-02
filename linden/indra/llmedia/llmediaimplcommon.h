713001efd41390bbcb28d071771ffae1201bab94
Jacek Antonelli <jacek.antonelli@gmail.com>
2009-04-01 23:15:29 -0500

    Added LLMediaImplCommon::setDebugLevel().

--- linden/indra/llmedia/llmediaimplcommon.h
+++ linden/indra/llmedia/llmediaimplcommon.h
@@ -54,6 +54,7 @@ class LLMediaImplCommon :
 		// housekeeping
 		virtual bool init();
 		virtual bool reset();
+		virtual bool setDebugLevel( LLMediaBase::EDebugLevel level );
 		virtual bool setMimeType( const std::string url );
 		virtual std::string getMimeType() const;
 		virtual std::string getMediaURL() const;
@@ -156,6 +157,7 @@ class LLMediaImplCommon :
 		LLMediaBase::ECommand mCommand;
 		LLMediaBase::EStatus mStatus;
 		bool mLooping;
+		LLMediaBase::EDebugLevel mDebugLevel;
 };
 
 #endif	// LLMEDIAIMPLCOMMON_H
