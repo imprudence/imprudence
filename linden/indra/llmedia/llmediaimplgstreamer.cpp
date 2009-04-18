c6f86609c7669d27f8ebad97ea6695384e17ee12
McCabe Maxsted <hakushakukun@gmail.com>
2009-04-17 20:35:24 -0700

    GST_PLUGIN_PATH should only search plugin directories for Windows

--- linden/indra/llmedia/llmediaimplgstreamer.cpp
+++ linden/indra/llmedia/llmediaimplgstreamer.cpp
@@ -291,10 +291,10 @@ void LLMediaImplGStreamer::set_gst_plugin_path()
 	// But we also want to search the path the user has set, if any.
 	std::string plugin_path =	
 		"GST_PLUGIN_PATH=" +
-		imp_dir + separator +
 #if LL_WINDOWS
 		imp_dir + "\\lib\\gstreamer-plugins" +
 #elif LL_DARWIN
+		imp_dir + separator +
 		imp_dir + "/../Resources/lib/gstreamer-plugins" +
 #endif
 		old_plugin_path;
