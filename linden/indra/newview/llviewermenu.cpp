287284331af10af8a55914d00b2c61d5fd40642f
McCabe Maxsted <hakushakukun@gmail.com>
2009-09-09 16:57:26 -0700

    Applied ReinstateShowTextureUUID Cool Viewer patch (reverts the fix for VWR-1919)

--- linden/indra/newview/llviewermenu.cpp
+++ linden/indra/newview/llviewermenu.cpp
@@ -6085,7 +6085,10 @@ void handle_selected_texture_info(void*)
 			S32 height = img->getHeight();
 			S32 width = img->getWidth();
 			S32 components = img->getComponents();
-			msg = llformat("%dx%d %s on face ",
+			std::string image_id_string = image_id.asString();
+			image_id_string = image_id_string.replace(24, 35, 12, '*') + " "; // hide last segment to discourage theft
+			msg = llformat("%s%dx%d %s on face ",
+								image_id_string.c_str(),
 								width,
 								height,
 								(components == 4 ? "alpha" : "opaque"));
