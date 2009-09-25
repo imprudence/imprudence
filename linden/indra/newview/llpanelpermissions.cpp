541e4cd5dc920e21357f97a15ce375a1592edea4
McCabe Maxsted <hakushakukun@gmail.com>
2009-09-25 00:07:55 -0700

    Fixed Last Owner text in tools window not disabling

--- linden/indra/newview/llpanelpermissions.cpp
+++ linden/indra/newview/llpanelpermissions.cpp
@@ -353,12 +353,14 @@ void LLPanelPermissions::refresh()
 	if ( (owner_name != last_owner_name) && (!gRlvHandler.hasBehaviour(RLV_BHVR_SHOWNAMES)) )
 // [/RLVa:KB]
 	{
+		childSetEnabled("Last Owner:", TRUE);
 		childSetText("Last Owner Name", last_owner_name);
 		childSetEnabled("Last Owner Name", TRUE);
 		childSetEnabled("button last owner profile", TRUE);
 	}
 	else
 	{
+		childSetEnabled("Last Owner:", FALSE);
 		childSetText("Last Owner Name", LLStringUtil::null);
 		childSetEnabled("Last Owner Name", FALSE);
 		childSetEnabled("button last owner profile", FALSE);
