7336d5883d5cb7b33e59de298d90280454a500fa
Patrick Sapinski <patrick@sapinski.com>
2009-11-25 04:01:02 -0500

    updated boost version strings for cmake

--- linden/indra/cmake/Boost.cmake
+++ linden/indra/cmake/Boost.cmake
@@ -15,7 +15,7 @@ else (STANDALONE)
   set(Boost_INCLUDE_DIRS ${LIBS_PREBUILT_DIR}/include)
 
   if (WINDOWS)
-    set(BOOST_VERSION 1_34_1)
+    set(BOOST_VERSION 1_35)
     if (MSVC71)
       set(BOOST_PROGRAM_OPTIONS_LIBRARY 
           optimized libboost_program_options-vc71-mt-s-${BOOST_VERSION}
@@ -28,14 +28,14 @@ else (STANDALONE)
           debug libboost_signals-vc71-mt-sgd-${BOOST_VERSION})
     else (MSVC71)
       set(BOOST_PROGRAM_OPTIONS_LIBRARY 
-          optimized libboost_program_options-vc80-mt-s-${BOOST_VERSION}
-          debug libboost_program_options-vc80-mt-sgd-${BOOST_VERSION})
+          optimized libboost_program_options-vc90-mt-s-${BOOST_VERSION}
+          debug libboost_program_options-vc90-mt-sgd-${BOOST_VERSION})
       set(BOOST_REGEX_LIBRARY
-          optimized libboost_regex-vc80-mt-s-${BOOST_VERSION}
-          debug libboost_regex-vc80-mt-sgd-${BOOST_VERSION})
+          optimized libboost_regex-vc90-mt-s-${BOOST_VERSION}
+          debug libboost_regex-vc90-mt-sgd-${BOOST_VERSION})
       set(BOOST_SIGNALS_LIBRARY 
-          optimized libboost_signals-vc80-mt-s-${BOOST_VERSION}
-          debug libboost_signals-vc80-mt-sgd-${BOOST_VERSION})
+          optimized libboost_signals-vc90-mt-s-${BOOST_VERSION}
+          debug libboost_signals-vc90-mt-sgd-${BOOST_VERSION})
     endif (MSVC71)
   elseif (DARWIN)
     set(BOOST_PROGRAM_OPTIONS_LIBRARY boost_program_options-mt)
