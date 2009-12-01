18b5636b74d80034867e2e8273706db4dc490b15
McCabe Maxsted <hakushakukun@gmail.com>
2009-12-01 08:14:11 -0700

    Made downloading the artwork work while using the --standalone flag

--- linden/indra/cmake/00-Common.cmake
+++ linden/indra/cmake/00-Common.cmake
@@ -5,10 +5,15 @@
 include(Variables)
 include(Prebuilt)
 
-# Download viewer artwork
+# Download viewer artwork even when using standalone
 
+if (STANDALONE)
+  set(STANDALONE OFF)
+    use_prebuilt_binary(artwork)
+  set(STANDALONE ON)
+else (STANDALONE)
   use_prebuilt_binary(artwork)
-
+endif (STANDALONE)
 
 # Portable compilation flags.
 
