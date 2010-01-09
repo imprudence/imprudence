ced750d70538b846b5943c4ca1541c3fa7d14630
Jacek Antonelli <jacek.antonelli@gmail.com>
2010-01-09 15:36:34 -0600

    Improved "Pay Resident" floater layout and XML.

--- linden/indra/newview/llgivemoney.cpp
+++ linden/indra/newview/llgivemoney.cpp
@@ -147,7 +147,9 @@ LLFloaterPay::LLFloaterPay(const std::string& name,
 	++i;
 
 	
-    childSetVisible("amount text", FALSE);	
+	childSetVisible("amount text", FALSE);	
+	childSetVisible("currency text", FALSE);
+
 
 	std::string last_amount;
 	if(sLastAmount > 0)
@@ -155,7 +157,7 @@ LLFloaterPay::LLFloaterPay(const std::string& name,
 		last_amount = llformat("%d", sLastAmount);
 	}
 
-    childSetVisible("amount", FALSE);
+	childSetVisible("amount", FALSE);
 	
 	childSetKeystrokeCallback("amount", &LLFloaterPay::onKeystroke, this);
 	childSetText("amount", last_amount);
@@ -207,12 +209,15 @@ void LLFloaterPay::processPayPriceReply(LLMessageSystem* msg, void **userdata)
 			self->childSetVisible("amount", FALSE);
 			self->childSetVisible("pay btn", FALSE);
 			self->childSetVisible("amount text", FALSE);
+			self->childSetVisible("currency text", FALSE);
+
 		}
 		else if (PAY_PRICE_DEFAULT == price)
 		{			
 			self->childSetVisible("amount", TRUE);
 			self->childSetVisible("pay btn", TRUE);
 			self->childSetVisible("amount text", TRUE);
+			self->childSetVisible("currency text", TRUE);
 		}
 		else
 		{
@@ -223,6 +228,7 @@ void LLFloaterPay::processPayPriceReply(LLMessageSystem* msg, void **userdata)
 			self->childSetVisible("pay btn", TRUE);
 			self->childSetEnabled("pay btn", TRUE);
 			self->childSetVisible("amount text", TRUE);
+			self->childSetVisible("currency text", TRUE);
 
 			self->childSetText("amount", llformat("%d", llabs(price)));
 		}
@@ -362,6 +368,7 @@ void LLFloaterPay::payDirectly(money_callback callback,
 	floater->childSetVisible("amount", TRUE);
 	floater->childSetVisible("pay btn", TRUE);
 	floater->childSetVisible("amount text", TRUE);
+	floater->childSetVisible("currency text", TRUE);
 
 	floater->childSetVisible("fastpay text",TRUE);
 	for(S32 i=0;i<MAX_PAY_BUTTONS;++i)
