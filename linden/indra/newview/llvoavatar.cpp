e56caa9c48b1b209f349cda88886d20f91407adf
Jacek Antonelli <jacek.antonelli@gmail.com>
2010-02-18 19:16:41 -0600

    SNOW-488: Malformed animation crash.
    Patch by Robin Cornelius.

--- linden/indra/newview/llvoavatar.cpp
+++ linden/indra/newview/llvoavatar.cpp
@@ -5306,7 +5306,7 @@ LLVector3 LLVOAvatar::getVolumePos(S32 joint_index, LLVector3& volume_offset)
 //-----------------------------------------------------------------------------
 LLJoint* LLVOAvatar::findCollisionVolume(U32 volume_id)
 {
-	if ((S32)volume_id > mNumCollisionVolumes)
+	if ((S32)volume_id > mNumCollisionVolumes || (S32)volume_id < 0)
 	{
 		return NULL;
 	}
