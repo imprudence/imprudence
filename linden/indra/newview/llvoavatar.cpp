693e7eabe0d824cd8ab243ed3b7540ac32eb5d11
Armin Weatherwax <Armin.Weatherwax@gmail.com>
2010-04-05 12:11:08 +0200

    fix: don't llerr-crash on some bad textures.
    Downside: instead of crashing some avatars stay white for some time.

--- linden/indra/newview/llvoavatar.cpp
+++ linden/indra/newview/llvoavatar.cpp
@@ -8579,7 +8579,7 @@ void LLVOAvatar::onBakedTextureMasksLoaded( BOOL success, LLViewerImage *src_vi,
 		{
 			if (!aux_src->getData())
 			{
-				llerrs << "No auxiliary source data for onBakedTextureMasksLoaded" << llendl;
+				llwarns << "No auxiliary source data for onBakedTextureMasksLoaded" << llendl;
 				return;
 			}
 
