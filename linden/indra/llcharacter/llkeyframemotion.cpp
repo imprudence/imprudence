5d05f7006ef4b6a1a13d4b45b3ae1ffa0dafb76a
McCabe Maxsted <hakushakukun@gmail.com>
2010-04-30 01:17:35 -0700

    Applied patch for SNOW-484 by Robin Cornelius: recursive animations can crash viewer

--- linden/indra/llcharacter/llkeyframemotion.cpp
+++ linden/indra/llcharacter/llkeyframemotion.cpp
@@ -1246,6 +1246,12 @@ BOOL LLKeyframeMotion::deserialize(LLDataPacker& dp)
 		return FALSE;
 	}
 
+	if(mJointMotionList->mEmoteName==mID.asString())
+	{
+		llwarns << "Malformed animation mEmoteName==mID" << llendl;
+		return FALSE;
+	}
+
 	//-------------------------------------------------------------------------
 	// get loop
 	//-------------------------------------------------------------------------
