e3dbb6a98909b68ec54766f82a3dd15fc247f30d
Armin Weatherwax <Armin.Weatherwax@gmail.com>
2010-05-16 12:13:22 +0200

    Update openjpeg library to 1.4.0.565 on Linux (32 and 64).

--- linden/indra/newview/viewer_manifest.py
+++ linden/indra/newview/viewer_manifest.py
@@ -898,7 +898,7 @@ class Linux_i686Manifest(LinuxManifest):
             self.path("libuuid.so", "libuuid.so.1")
             self.path("libSDL-1.2.so.0")
             self.path("libELFIO.so")
-            self.path("libopenjpeg.so.1.3.0", "libopenjpeg.so.1.3")
+            self.path("libopenjpeg.so.2")
             self.path("libxml2.so.2")
             self.path("libz.so.1")
 
