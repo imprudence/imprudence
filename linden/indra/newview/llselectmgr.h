64390436e0c46877bd61aa2965f405c9bc705e6e
McCabe Maxsted <hakushakukun@gmail.com>
2010-05-31 22:59:47 -0700

    Updated llselectmgr.cpp to snowglobe 1.4

--- linden/indra/newview/llselectmgr.h
+++ linden/indra/newview/llselectmgr.h
@@ -138,6 +138,7 @@ public:
 	void selectTE(S32 te_index, BOOL selected);
 	BOOL isTESelected(S32 te_index);
 	S32 getLastSelectedTE();
+	S32 getTESelectMask() { return mTESelectMask; }
 	void renderOneSilhouette(const LLColor4 &color);
 	void setTransient(BOOL transient) { mTransient = transient; }
 	BOOL isTransient() { return mTransient; }
@@ -190,7 +191,7 @@ public:
 
 protected:
 	LLPointer<LLViewerObject>	mObject;
-	BOOL			mTESelected[SELECT_MAX_TES];
+	S32				mTESelectMask;
 	S32				mLastTESelected;
 };
 
@@ -406,7 +407,7 @@ public:
 	// converts all objects currently highlighted to a selection, and returns it
 	LLObjectSelectionHandle selectHighlightedObjects();
 
-	LLObjectSelectionHandle setHoverObject(LLViewerObject *objectp);
+	LLObjectSelectionHandle setHoverObject(LLViewerObject *objectp, S32 face = -1);
 
 	void highlightObjectOnly(LLViewerObject *objectp);
 	void highlightObjectAndFamily(LLViewerObject *objectp);
@@ -647,12 +648,14 @@ private:
 	ESelectType getSelectTypeForObject(LLViewerObject* object);
 	void addAsFamily(std::vector<LLViewerObject*>& objects, BOOL add_to_end = FALSE);
 	void generateSilhouette(LLSelectNode *nodep, const LLVector3& view_point);
+	void updateSelectionSilhouette(LLObjectSelectionHandle object_handle, S32& num_sils_genned, std::vector<LLViewerObject*>& changed_objects);
 	// Send one message to each region containing an object on selection list.
 	void sendListToRegions(	const std::string& message_name,
 							void (*pack_header)(void *user_data), 
 							void (*pack_body)(LLSelectNode* node, void *user_data), 
 							void *user_data,
 							ESendType send_type);
+
 
 	static void packAgentID(	void *);
 	static void packAgentAndSessionID(void* user_data);
