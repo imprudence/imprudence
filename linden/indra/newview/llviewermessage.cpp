8c9275764135e4bb60ed7ca62786f4af78cd31d5
McCabe Maxsted <hakushakukun@gmail.com>
2010-06-05 19:49:25 -0700

    Removed 'Received animation state for unknown avatar' log spam

--- linden/indra/newview/llviewermessage.cpp
+++ linden/indra/newview/llviewermessage.cpp
@@ -4260,7 +4260,7 @@ void process_avatar_animation(LLMessageSystem *mesgsys, void **user_data)
 	if (!avatarp)
 	{
 		// no agent by this ID...error?
-		LL_WARNS("Messaging") << "Received animation state for unknown avatar" << uuid << LL_ENDL;
+		LL_DEBUGS("Messaging") << "Received animation state for unknown avatar: " << uuid << LL_ENDL;
 		return;
 	}
 
