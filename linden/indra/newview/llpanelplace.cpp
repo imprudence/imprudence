fda39084e2baff093d6b520c3ab30375e734bb3c
McCabe Maxsted <hakushakukun@gmail.com>
2010-06-21 05:38:13 -0700

    Fixed #357: Crash in LLPanelPlace::onClickTeleport

--- linden/indra/newview/llpanelplace.cpp
+++ linden/indra/newview/llpanelplace.cpp
@@ -416,7 +416,10 @@ void LLPanelPlace::onClickTeleport(void* data)
 		parent_floaterp->close();
 	}
 	// LLFloater* parent_floaterp = (LLFloater*)self->getParent();
-	parent_viewp->setVisible(false);
+	if (parent_viewp)
+	{
+		parent_viewp->setVisible(false);
+	}
 	if(self->mLandmarkAssetID.notNull())
 	{
 		gAgent.teleportViaLandmark(self->mLandmarkAssetID);
