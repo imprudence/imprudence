d94100f39c568bef7da7ce429cd3c3f20a903aee
McCabe Maxsted <hakushakukun@gmail.com>
2010-06-23 20:46:07 -0700

    Allow map teleports up to the set grid height

--- linden/indra/newview/llfloaterworldmap.cpp
+++ linden/indra/newview/llfloaterworldmap.cpp
@@ -70,6 +70,8 @@
 
 #include "llglheaders.h"
 
+#include "hippoLimits.h"
+
 //---------------------------------------------------------------------------
 // Constants
 //---------------------------------------------------------------------------
@@ -779,7 +781,7 @@ void LLFloaterWorldMap::updateLocation()
 void LLFloaterWorldMap::trackURL(const std::string& region_name, S32 x_coord, S32 y_coord, S32 z_coord)
 {
 	LLSimInfo* sim_info = LLWorldMap::getInstance()->simInfoFromName(region_name);
-	z_coord = llclamp(z_coord, 0, 4096);
+	z_coord = llclamp(z_coord, 0, gHippoLimits->getMaxHeight());
 	if (sim_info)
 	{
 		LLVector3 local_pos;
