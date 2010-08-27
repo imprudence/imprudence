b909f56eaa571a95debd9e47ba486288ec46dbc6
McCabe Maxsted <hakushakukun@gmail.com>
2010-08-27 06:40:35 -0700

    Fixed object scale not working at 5 decimal places (still needs fixing for position and rotation)

--- linden/indra/newview/llpanelobject.cpp
+++ linden/indra/newview/llpanelobject.cpp
@@ -1718,7 +1718,7 @@ void LLPanelObject::sendRotation(BOOL btn_down)
 	// Note: must compare before conversion to radians
 	LLVector3 delta = new_rot - mCurEulerDegrees;
 
-	if (delta.magVec() >= 0.0001f)
+	if (delta.magVec() >= 0.00001f)
 	{
 		mCurEulerDegrees = new_rot;
 		new_rot *= DEG_TO_RAD;
@@ -1764,7 +1764,7 @@ void LLPanelObject::sendScale(BOOL btn_down)
 	LLVector3 newscale(mCtrlScaleX->get(), mCtrlScaleY->get(), mCtrlScaleZ->get());
 
 	LLVector3 delta = newscale - mObject->getScale();
-	if (delta.magVec() >= 0.0001f)
+	if (delta.magVec() >= 0.00001f)
 	{
 		// scale changed by more than 1/2 millimeter
 
@@ -1835,7 +1835,7 @@ void LLPanelObject::sendPosition(BOOL btn_down)
 		LLVector3d old_pos_global = mObject->getPositionGlobal();
 		LLVector3d delta = new_pos_global - old_pos_global;
 		// moved more than 1/2 millimeter
-		if (delta.magVec() >= 0.0001f)
+		if (delta.magVec() >= 0.00001f)
 		{			
 			if (mRootObject != mObject)
 			{
