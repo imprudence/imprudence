02913d28b3d9cc8805537afcbe4375fa21c313f3
Aleric Inglewood <Aleric.Inglewood@gmail.com>
2010-09-28 03:01:40 +0200

    RED-552: VWR-12838: NDOF is assumed to exist, even with --standalone.
    See http://redmine.imprudenceviewer.org/issues/552
    Add FindNDOF.cmake and treat it like every other library (on
    standalone).

--- linden/indra/cmake/NDOF.cmake
+++ linden/indra/cmake/NDOF.cmake
@@ -1,14 +1,28 @@
 # -*- cmake -*-
 include(Prebuilt)
 
-use_prebuilt_binary(ndofdev)
+if (STANDALONE)
+  include(FindNDOF)
+  if(NOT NDOF_FOUND)
+    message(STATUS "Building without N-DoF joystick support")
+  endif(NOT NDOF_FOUND)
+else (STANDALONE)
+  use_prebuilt_binary(ndofdev)
 
-if (WINDOWS OR DARWIN OR LINUX)
+  if (WINDOWS)
+    set(NDOF_LIBRARY libndofdev)
+  elseif (DARWIN OR LINUX)
+    set(NDOF_LIBRARY ndofdev)
+  endif (WINDOWS)
+
+  set(NDOF_INCLUDE_DIR ${ARCH_PREBUILT_DIRS}/include/ndofdev)
+  set(NDOF_FOUND 1)
+endif (STANDALONE)
+
+if (NDOF_FOUND)
   add_definitions(-DLIB_NDOF=1)
-endif (WINDOWS OR DARWIN OR LINUX)
-
-if (WINDOWS)
-  set(NDOF_LIBRARY libndofdev)
-elseif (DARWIN OR LINUX)
-  set(NDOF_LIBRARY ndofdev)
-endif (WINDOWS)
+  include_directories(${NDOF_INCLUDE_DIR})
+else (NDOF_FOUND)
+  set(NDOF_INCLUDE_DIR "")
+  set(NDOF_LIBRARY "")
+endif (NDOF_FOUND)
