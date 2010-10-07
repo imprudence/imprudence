imprudence/imprudence

commit f5006e8a2f34afa390f0bbcf0f6af15218cf4105

LLLineEditor context translator:  - Added a xui attribute "allow_translate", default to off (not enabled    in any xml file yet.)  - Only show translate menu if clicked on a word, or something is selected.

linden/indra/llui/lllineeditor.cpp
@@ -175,7 +175,8 @@ LLLineEditor::LLLineEditor(const std::string& name, const LLRect& rect,
 		mImage( sImage ),
 		mReplaceNewlinesWithSpaces( TRUE ),
 		mSpellCheckable( FALSE ),
-		mShowMisspellings(FALSE)
+		mShowMisspellings(FALSE),
+		mAllowTranslate(FALSE)
 {
 	llassert( max_length_bytes > 0 );
 
@@ -762,7 +763,8 @@ BOOL LLLineEditor::handleRightMouseDown( S32 x, S32 y, MASK mask )
 
 	//setCursorAtLocalPos( x);
 	S32 wordStart = 0;
-	S32 wordEnd = calculateCursorFromMouse(x);
+	S32 wordEnd = 0;
+	S32 pos = calculateCursorFromMouse(x);
 
 	LLMenuGL* menu = (LLMenuGL*)mPopupMenuHandle.get();
 	if (menu)
@@ -785,29 +787,19 @@ BOOL LLLineEditor::handleRightMouseDown( S32 x, S32 y, MASK mask )
 		}
 		suggestionMenuItems.clear();
 
-		menu->setItemVisible("Translate To", !mReadOnly);
-		menu->setItemVisible("Transep", !mReadOnly);
+		bool is_word_part = getWordBoundriesAt(pos, &wordStart, &wordEnd);
+		// allow_translate="true" in xui
+		bool can_translate = mAllowTranslate && !mReadOnly && (is_word_part || hasSelection());
+		menu->setItemVisible("Translate To", can_translate);
+		menu->setItemVisible("Transep", can_translate);
 
 		// spell_check="true" in xui
 		if (!mReadOnly && mSpellCheckable)
 		{
-			const LLWString& text = mText.getWString();
-
 			// search for word matches
-			if (LLTextEditor::isPartOfWord(text[wordEnd]))
+			if (is_word_part)
 			{
-				// Select word the cursor is over
-				while ((wordEnd > 0) && LLTextEditor::isPartOfWord(text[wordEnd-1]))
-				{
-					wordEnd--;
-				}
-				wordStart = wordEnd;
-				//startSelection();
-
-				while ((wordEnd < (S32)text.length()) && LLTextEditor::isPartOfWord( text[wordEnd] ) )
-				{
-					wordEnd++;
-				}		
+				const LLWString& text = mText.getWString();
 				std::string selectedWord(std::string(text.begin(), text.end()).substr(wordStart,wordEnd-wordStart));
 				
 				if (!glggHunSpell->isSpelledRight(selectedWord))
@@ -2892,6 +2884,11 @@ LLView* LLLineEditor::fromXML(LLXMLNodePtr node, LLView *parent, LLUICtrlFactory
 	if (node->getAttributeBOOL("spell_check", spell_checking))
 	{
 		line_editor->setSpellCheckable(spell_checking);
+	}
+	BOOL allow_translate = FALSE;
+	if (node->getAttributeBOOL("allow_translate", allow_translate))
+	{
+		line_editor->setAllowTranslate(allow_translate);
 	}
 	
 	line_editor->setColorParameters(node);

linden/indra/llui/lllineeditor.h
@@ -200,6 +200,7 @@ public:
 	void setReadOnlyBgColor( const LLColor4& c )	{ mReadOnlyBgColor = c; }
 	void setFocusBgColor(const LLColor4& c)			{ mFocusBgColor = c; }
 	void setSpellCheckable(BOOL b)					{ mSpellCheckable = b; }
+	void setAllowTranslate(BOOL b)					{ mAllowTranslate = b; }
 
 	const LLColor4& getFgColor() const			{ return mFgColor; }
 	const LLColor4& getReadOnlyFgColor() const	{ return mReadOnlyFgColor; }
@@ -301,6 +302,7 @@ protected:
 	//to keep track of what we have to remove before showing menu
 	std::vector<SpellMenuBind* > suggestionMenuItems;
 	S32 mLastContextMenuX;
+	BOOL			mAllowTranslate;		// set in xui as "allow_translate".
 
 	// line history support:
 	BOOL		mHaveHistory;				// flag for enabled line history

commit 1eee67b68de88e1f2a7e0f1d55b3e993e819a419

Fix bug #611: Context menu spell checker suggestions broken.
This fixes a confusion between word end position and word length,
causing an empty spell checker suggestion list, or possibly crashes.

I caused this bug in f5006e8a2f34afa390f0bbcf0f6af15218cf4105.
Oops.

linden/indra/llui/lllineeditor.cpp
@@ -779,7 +779,7 @@ BOOL LLLineEditor::handleRightMouseDown( S32 x, S32 y, MASK mask )
 
 	//setCursorAtLocalPos( x);
 	S32 wordStart = 0;
-	S32 wordEnd = 0;
+	S32 wordLen = 0;
 	S32 pos = calculateCursorFromMouse(x);
 
 	LLMenuGL* menu = (LLMenuGL*)mPopupMenuHandle.get();
@@ -808,11 +808,11 @@ BOOL LLLineEditor::handleRightMouseDown( S32 x, S32 y, MASK mask )
 		if (!mReadOnly && mSpellCheckable)
 		{
 			// search for word matches
-			bool is_word_part = getWordBoundriesAt(pos, &wordStart, &wordEnd);
+			bool is_word_part = getWordBoundriesAt(pos, &wordStart, &wordLen);
 			if (is_word_part)
 			{
 				const LLWString& text = mText.getWString();
-				std::string selectedWord(std::string(text.begin(), text.end()).substr(wordStart,wordEnd-wordStart));
+				std::string selectedWord(std::string(text.begin(), text.end()).substr(wordStart,wordLen));
 				
 				if (!glggHunSpell->isSpelledRight(selectedWord))
 				{	
@@ -824,7 +824,7 @@ BOOL LLLineEditor::handleRightMouseDown( S32 x, S32 y, MASK mask )
 						SpellMenuBind * tempStruct = new SpellMenuBind;
 						tempStruct->origin = this;
 						tempStruct->word = suggs[i];
-						tempStruct->wordPositionEnd = wordEnd;
+						tempStruct->wordPositionEnd = wordStart + wordLen;
 						tempStruct->wordPositionStart=wordStart;
 						LLMenuItemCallGL * suggMenuItem = new LLMenuItemCallGL(
 							tempStruct->word, spell_correct, NULL, tempStruct);
@@ -836,7 +836,7 @@ BOOL LLLineEditor::handleRightMouseDown( S32 x, S32 y, MASK mask )
 					SpellMenuBind * tempStruct = new SpellMenuBind;
 					tempStruct->origin = this;
 					tempStruct->word = selectedWord;
-					tempStruct->wordPositionEnd = wordEnd;
+					tempStruct->wordPositionEnd = wordStart + wordLen;
 					tempStruct->wordPositionStart=wordStart;
 					LLMenuItemCallGL * suggMenuItem = new LLMenuItemCallGL(
 						"Add Word", spell_add, NULL, tempStruct);

linden/indra/llui/lltexteditor.cpp
@@ -1502,7 +1502,7 @@ BOOL LLTextEditor::handleRightMouseDown( S32 x, S32 y, MASK mask )
 
 	//setCursorAtLocalPos( x, y, TRUE );
 	S32 wordStart = 0;
-	S32 wordEnd = 0;
+	S32 wordLen = 0;
 	S32 pos = getCursorPosFromLocalCoord(x,y,TRUE);
 
 	LLMenuGL* menu = (LLMenuGL*)mPopupMenuHandle.get();
@@ -1526,11 +1526,11 @@ BOOL LLTextEditor::handleRightMouseDown( S32 x, S32 y, MASK mask )
 		menu->setItemVisible("Spelsep", !mReadOnly && mSpellCheckable);
 		if (!mReadOnly && mSpellCheckable)
 		{
-			bool is_word_part = getWordBoundriesAt(pos, &wordStart, &wordEnd);
+			bool is_word_part = getWordBoundriesAt(pos, &wordStart, &wordLen);
 			if (is_word_part)
 			{
 				const LLWString &text = mWText;
-				std::string selectedWord(std::string(text.begin(), text.end()).substr(wordStart,wordEnd-wordStart));
+				std::string selectedWord(std::string(text.begin(), text.end()).substr(wordStart,wordLen));
 
 				if (!glggHunSpell->isSpelledRight(selectedWord))
 				{
@@ -1542,7 +1542,7 @@ BOOL LLTextEditor::handleRightMouseDown( S32 x, S32 y, MASK mask )
 						SpellMenuBind * tempStruct = new SpellMenuBind;
 						tempStruct->origin = this;
 						tempStruct->word = suggs[i];
-						tempStruct->wordPositionEnd = wordEnd;
+						tempStruct->wordPositionEnd = wordStart + wordLen;
 						tempStruct->wordPositionStart=wordStart;
 						tempStruct->wordY=y;
 						LLMenuItemCallGL * suggMenuItem = new LLMenuItemCallGL(
@@ -1554,7 +1554,7 @@ BOOL LLTextEditor::handleRightMouseDown( S32 x, S32 y, MASK mask )
 					SpellMenuBind * tempStruct = new SpellMenuBind;
 					tempStruct->origin = this;
 					tempStruct->word = selectedWord;
-					tempStruct->wordPositionEnd = wordEnd;
+					tempStruct->wordPositionEnd = wordStart + wordLen;
 					tempStruct->wordPositionStart=wordStart;
 					tempStruct->wordY=y;
 					LLMenuItemCallGL * suggMenuItem = new LLMenuItemCallGL(