17c1840637925c1165e434b502d09ee82a3d4942
thickbrick <thickbrick.sleaford@gmail.com>
2010-10-16 03:16:14 +0200

    Port of SNOW-802: Use UDP for baked textures.
    Port of SNOW-802 from SG1. Originally ported by Robin cornelius from
    SG 2.1.
    
    Also added a paramter can_use_http to the LLTextureFetchWorker
    constructor, to avoid messing with locks.

--- linden/indra/newview/lltexturefetch.h
+++ linden/indra/newview/lltexturefetch.h
@@ -60,7 +60,7 @@ public:
 	/*virtual*/ S32 update(U32 max_time_ms);	
 
 	bool createRequest(const std::string& url, const LLUUID& id, const LLHost& host, F32 priority,
-					   S32 w, S32 h, S32 c, S32 discard, bool needs_aux);
+					   S32 w, S32 h, S32 c, S32 discard, bool needs_aux, bool use_http);
 	void deleteRequest(const LLUUID& id, bool cancel);
 	bool getRequestFinished(const LLUUID& id, S32& discard_level,
 							LLPointer<LLImageRaw>& raw, LLPointer<LLImageRaw>& aux);
