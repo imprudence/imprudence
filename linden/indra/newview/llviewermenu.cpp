02d06e7cf7c15ddd427197dbe177ea58ffc91319
thickbrick <thickbrick.sleaford@gmail.com>
2011-01-20 01:34:15 +0200

    Add menu option for sculpt debugging in Advanced menu.
    The new option is Advanced -> Rendering -> Info Displays -> Sculpt.
    
    The code for displaying this debug info is old, but the menu option was
    lost when the Advanced menu was converted to xml.

--- linden/indra/newview/llviewermenu.cpp
+++ linden/indra/newview/llviewermenu.cpp
@@ -9192,6 +9192,10 @@ U32 info_display_from_string(std::string info_display)
 	{
 		return LLPipeline::RENDER_DEBUG_GLOW;
 	}
+	else if ("sculpt" == info_display)
+	{
+		return LLPipeline::RENDER_DEBUG_SCULPTED;
+	}
 	else
 	{
 		return 0;
