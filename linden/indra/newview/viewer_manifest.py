74d6849a9730941839f00c1ec2b640958fd61cc9
Jacek Antonelli <jacek@imprudenceviewer.org>
2011-03-19 13:38:38 -0500

    Package both libz.so.1 and libz.so (symlink) on Linux32. (IMP-790)
    The viewer binary links against libz.so, but libvivoxsdk.so and some
    other libraries link against libz.so.1.

--- linden/indra/newview/viewer_manifest.py
+++ linden/indra/newview/viewer_manifest.py
@@ -1018,7 +1018,8 @@ class Linux_i686Manifest(LinuxManifest):
             self.path("libELFIO.so")
             self.path("libopenjpeg.so.2")
             self.path("libxml2.so.2")
-            self.path("libz.so.1","libz.so")
+            self.path("libz.so")
+            self.path("libz.so.1")
 
             # OpenAL
             self.path("libopenal.so.1.12.854", "libopenal.so.1")
