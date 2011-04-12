9a3f5a47f872288a373b0919b78ee042529e4327
McCabe Maxsted <McCabe.Maxsted@gmail.com>
2011-04-12 02:21:04 -0700

    URLs are now clickable in group charters

--- linden/indra/newview/llpanelgroupgeneral.cpp
+++ linden/indra/newview/llpanelgroupgeneral.cpp
@@ -56,6 +56,7 @@
 #include "lltextbox.h"
 #include "lltexteditor.h"
 #include "lltexturectrl.h"
+#include "llviewercontrol.h"
 #include "llviewerwindow.h"
 
 // consts
@@ -813,7 +814,11 @@ void LLPanelGroupGeneral::update(LLGroupChange gc)
 
 	if (mEditCharter)
 	{
-		mEditCharter->setText(gdatap->mCharter);
+		mEditCharter->setParseHTML(TRUE);
+		mEditCharter->appendColoredText(gdatap->mCharter, false, false, 
+																		(mAllowEdit && can_change_ident) ? 
+																		gColors.getColor("TextFgColor") :
+																		gColors.getColor("TextFgReadOnlyColor"));
 		mEditCharter->resetDirty();
 	}
 	
