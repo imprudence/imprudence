6320754272fca9cdd1b0d993b426644d7ca4101a
McCabe Maxsted <McCabe.Maxsted@gmail.com>
2011-04-19 17:43:34 -0700

    Applied fix for crash when item just added to the inventory is renamed, ported from Phoenix, patch by Henri Beauchamp and Ansariel Hiller

--- linden/indra/newview/llviewerinventory.cpp
+++ linden/indra/newview/llviewerinventory.cpp
@@ -165,10 +165,9 @@ void LLViewerInventoryItem::updateServer(BOOL is_new) const
 {
 	if(!mIsComplete)
 	{
-		// *FIX: deal with this better.
-		// If we're crashing here then the UI is incorrectly enabled.
-		llerrs << "LLViewerInventoryItem::updateServer() - for incomplete item"
-			   << llendl;
+		llwarns << "LLViewerInventoryItem::updateServer() - for incomplete item"
+				<< llendl;
+		LLNotifications::instance().add("IncompleteInventoryItem");
 		return;
 	}
 	if(gAgent.getID() != mPermissions.getOwner())
